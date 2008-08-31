bf2d0043e20594e8f1ca8b3560e0c20df230583c
afex <afex@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-31 12:19:51 +0000

    synced with wakoopa badges
    git-svn-id: https://plugins.svn.wordpress.org/wakoopa-widget/trunk@62277 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: afex
 Tags: wakoopa, widget
 Requires at least: 2.0 or higher
-Tested up to: 2.2
-Stable tag: 0.1b
+Tested up to: 2.6.1
+Stable tag: 0.2b
 
 The wakoopa widget allows you to easily show your readers what software you frequently use via the wakoopa service.
 
@@ -17,5 +17,5 @@ Use this widget if you don't want to edit your theme manually in order to includ
 
 1. Upload `wakoopa.php` to the `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
-1. Add the widget to your theme via the Presentation -> Widgets screen
+1. Add the widget to your theme via the Design -> Widgets screen
 1. Be sure to input your wakoopa username into the settings for the widget
--- wakoopa.php
+++ wakoopa.php
@@ -26,14 +26,32 @@ function wakoopa_widget_init() {
 		$options = get_option('widget_wakoopa');
 		$title = $options['title'];
 		$username = $options['username'];
-        $use_image = $options['use_image'];
+        $use_text = $options['use_text'];
+        $list_type = $options['list_type'];
         
 		echo $before_widget . $before_title . $title . $after_title;
 		if ($username) {
-            if ($use_image) {
-    		    ?>
-    		    <a href="http://wakoopa.com/<?=$username?>"><img src="http://wakoopa.com/<?=$username?>/badge" alt="My top 10 software" /></a>
-    		    <?
+            if (!$use_text) {
+                switch ($list_type) {                        
+                    case 'recent':
+                        ?>
+            		    <a href="http://wakoopa.com/<?=$username?>"><img src="http://wakoopa.com/<?=$username?>/badge/recent.png" alt="My recently used software" title="My recently used software" /></a>
+            		    <?
+                        break;
+                        
+                    case 'new':
+                        ?>
+            		    <a href="http://wakoopa.com/<?=$username?>"><img src="http://wakoopa.com/<?=$username?>/badge/new.png" alt="My newly used software" title="My newly used software" /></a>
+            		    <?
+                        break;
+                        
+                    case 'top':
+                    default:
+            		    ?>
+            		    <a href="http://wakoopa.com/<?=$username?>"><img src="http://wakoopa.com/<?=$username?>/badge.png" alt="My top 10 software" title="My top 10 software" /></a>
+            		    <?
+                        break;
+                }
     		} else {
     		    ?>
     		    <h2><a href="http://wakoopa.com/<?=$username?>">My top 10 software</a></h2><script type="text/javascript" src="http://wakoopa.com/<?=$username?>/widget"></script>
@@ -51,19 +69,20 @@ function wakoopa_widget_init() {
 		$options = get_option('widget_wakoopa');
 
 		if ( !is_array($options) )
-			$options = array('title'=>'wakoopa','username'=>'', 'use_image'=>true);
+			$options = array('title'=>'wakoopa','username'=>'', 'use_text'=>false);
 
 		if ( $_POST['wakoopa-submit'] ) {
 			$options['title'] = strip_tags(stripslashes($_POST['wakoopa-title']));
 			$options['username'] = strip_tags(stripslashes($_POST['wakoopa-username']));
-			$options['use_image'] = ($_POST['wakoopa-image']?true:false);
+			$options['use_text'] = ($_POST['wakoopa-text']?true:false);
+			$options['list_type'] = $_POST['wakoopa-list-type'];
 			update_option('widget_wakoopa', $options);
 		}
 
 		$title = htmlspecialchars($options['title'], ENT_QUOTES);
 		$username = htmlspecialchars($options['username'], ENT_QUOTES);
-		$use_image = $options['use_image'];
-		
+		$use_text = $options['use_text'];
+		$list_type = $options['list_type'];
 		?>
 		<p style="text-align:left;">
 		    <label for="wakoopa-title"><?=__('Title:')?>
@@ -76,9 +95,14 @@ function wakoopa_widget_init() {
 		        <i>Don't have a wakoopa account? <a href="http://wakoopa.com/account/signup">Get one here</a></i>
 		    </label>
 		</p>
+		<p style='text-align:left;'>
+		    <input type="radio" name="wakoopa-list-type" value="top" id="wakoopa-list-type-top"<?= ($list_type=='top' ? ' checked' : '') ?>><label for="wakoopa-list-type-top">Top 10</label><br/>
+		    <input type="radio" name="wakoopa-list-type" value="recent" id="wakoopa-list-type-recent"<?= ($list_type=='recent' ? ' checked' : '') ?>><label for="wakoopa-list-type-recent">Recently used</label><br/>
+		    <input type="radio" name="wakoopa-list-type" value="new" id="wakoopa-list-type-new"<?= ($list_type=='new' ? ' checked' : '') ?>><label for="wakoopa-list-type-new">Newly used</label><br/>
+		</p>
 		<p style="text-align:left;">
-		    <label for="wakoopa-image"><?=__('Use badge image? (unchecked for plain text):')?>
-		        <input id="wakoopa-image" name="wakoopa-image" type="checkbox" <?=($use_image?'checked':'')?> />
+		    <label for="wakoopa-text"><?=__('Use text instead of image? (Top 10 only)')?>
+		        <input id="wakoopa-text" name="wakoopa-text" type="checkbox" <?=($use_text?'checked':'')?> />
 		    </label>
 		</p>
 		<input type="hidden" id="wakoopa-submit" name="wakoopa-submit" value="1" />
